34175d6ca6d157b26c0f9455b0179a1a161dd9c6
Benjamim Sonntag <benjamimsonntag@gmail.com>
2018-10-27 18:18:28 +0100

    v0.0.0-alpha.1

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = require("react");
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function useCounter() {
+  var initialState = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
+  var _useState = (0, _react.useState)(initialState),
+      _useState2 = _slicedToArray(_useState, 2),
+      state = _useState2[0],
+      setState = _useState2[1];
+
+  var increment = function increment() {
+    return setState(function (state) {
+      return state + 1;
+    });
+  };
+
+  return [state, increment];
+}
+
+var _default = useCounter;
+exports.default = _default;
